b659143efc4b1dc64e99fee6f9cbcb01240c1f55
Gumma Ram Sandeep <ramsandeepgumma@gmail.com>
2026-03-18 20:08:44 +0530

    Update AVL_tree.cpp
    in LAB

--- AVL_tree.cpp
+++ AVL_tree.cpp
@@ -3,34 +3,33 @@ using namespace std;
 struct node{
     int ID;
     int rating;
-    char name[50];
+    string name;
     node *left,*right;
 };
-struct node* createNode(int ID){
+struct node* createNode(int ID,string& name,int rating){
     node *new_node;
     new_node= new node;
     new_node->ID=ID;
-    cout<<"Enter the name and rating: ";
-    cin>>new_node->name>>new_node->rating;
-    
+    new_node->name=name;
+    new_node->rating=rating;
     new_node->left=NULL;
     new_node->right=NULL;
     return new_node;
 }
-void insert_node(node**root,int ID){
+void insert_node(node**root,int ID,string& name,int rating){
     if(*root==NULL){
-     *root=createNode(ID);
+     *root=createNode(ID,name,rating);
      return;
  }  
  if(ID==(*root)->ID){
     return;
  }
  else if(ID<(*root)->ID){
-    insert_node(&(*root)->left,ID);
+    insert_node(&(*root)->left,ID,name,rating);
    
  }
   else if(ID>(*root)->ID){
-    insert_node(&(*root)->right,ID);
+    insert_node(&(*root)->right,ID,name,rating);
    
  }
 
@@ -40,7 +39,7 @@ if(root==NULL){
     return;
 }
   display_tree_inorder(root->left);
-  cout<<"ID: "<<root->ID<<" Name: "<<root->name<<" Rating: "<<root->rating<<endl;
+  cout<<"ID:"<<root->ID<<" Name:"<<root->name<<" Rating:"<<root->rating<<"/10"<<endl;
   display_tree_inorder(root->right);
  
 }
@@ -85,7 +84,7 @@ void search_ID(node *root,int ID){
     return;
   }
   else if(root->ID==ID){
-    cout<<"ID: "<<root->ID<<" Name: "<<root->name<<" Rating: "<<root->rating;
+    cout<<"Employee Found -> ID:"<<root->ID<<" Name:"<<root->name<<" Rating:"<<root->rating<<"/10";
     return;
   }
   else if(ID>root->ID){
@@ -96,8 +95,12 @@ void search_ID(node *root,int ID){
   }
 }
 void update_rating(node **root,int ID,int rating){
-  if (root==NULL){
+  if (*root==NULL){
     cout<<"ID Not found";
+    return;
+  }
+  else if(rating>10||rating<0){
+    cout<<"Rating must be between 0 and 10";
     return;
   }
   else if((*root)->ID==ID){
@@ -129,12 +132,16 @@ bool check_unbalance(node *root){
       return false;
     }
 }
-void AVL_tree(node **root,int ID){
-  if(root==NULL||(*root)->ID==ID){
+int balance_factor(node*root){
+    int x=find_height(root->left)-find_height(root->right);
+      return x;
+}
+void AVL_tree_insertion(node **root,int ID){
+  if(*root==NULL||(*root)->ID==ID){
     return;
   }
  if(ID<(*root)->ID){
- AVL_tree(&(*root)->left,ID);
+ AVL_tree_insertion(&(*root)->left,ID);
   if(check_unbalance((*root))){
    if((*root)->left!=NULL&&ID<(*root)->left->ID)
    LL_rotation(root);
@@ -143,37 +150,117 @@ void AVL_tree(node **root,int ID){
   }
 }
  else if(ID>(*root)->ID)
-  AVL_tree(&(*root)->right,ID);
+  AVL_tree_insertion(&(*root)->right,ID);
 if(check_unbalance(*root)){
    if((*root)->right!=NULL&&ID>(*root)->right->ID)
    RR_rotation(root);
    else
    RL_rotation(root);
   }
- 
   
-   
+}
+void AVL_tree_deletion(node **root){
+  if(*root==NULL){
+    return;
+  }
+
+    if(balance_factor(*root)==2){
+      if(balance_factor((*root)->left)!=-1){
+        LL_rotation(root);
+      }
+      else{
+        LR_rotation(root);
+      }
+    }
+    else if(balance_factor(*root)==-2){
+       if(balance_factor((*root)->right)!=1){
+        RR_rotation(root);
+      }
+      else{
+        RL_rotation(root);
+      }
+    }
+  
+}
+void delete_node2(node **root,int x);
+
+int find_min(node* root){
+    if(root->left==NULL){
+        return root->ID;
+    }
+    else{
+       return find_min(root->left);
+    }
+
+}
+void case_1(node **root){
+    node *temp;
+    temp=*root;
+    *root=NULL;
+    delete temp;
+}
+void case_2(node **root){
+    node *temp;
+    temp=*root;
+    if((*root)->left!=NULL)
+    *root=(*root)->left;
+    else
+     *root=(*root)->right;
+     delete(temp);
+}
+void case_3(node **root){
+    int y=find_min((*root)->right);
+    (*root)->ID=y;
+    delete_node2(root,y);
+}
+void delete_node2(node **root,int ID){
+    if(*root==NULL){
+        cout<<"ID not found";
+        return;
+    }
+    else if(ID<(*root)->ID){
+        delete_node2(&(*root)->left,ID);
+    }
+    else if(ID>(*root)->ID){
+        delete_node2(&((*root)->right),ID);
+    }
+    else if(ID==(*root)->ID){
+        if((*root)->left==NULL&&(*root)->right==NULL){
+            case_1(root);
+        }
+        else if(((*root)->left!=NULL&&(*root)->right==NULL)||((*root)->left==NULL&&(*root)->right!=NULL)){
+            case_2(root);
+        }
+        else if((*root)->left!=NULL&&(*root)->right!=NULL){
+            case_3(root);
+        }
+    }
 }
 int main(){
     node *root;
     root=NULL;
     int input,n;
-    cout<<"1.Employee Join\n2.Employee Exit\n3.Search Employee\n4.Update Rating\n5.Display Employees\n6.Exit\nEnter your choice: ";
+    cout<<"AVL Tree Based Employee Perfomane Management System Menu\n1.Employee Join\n2.Employee Exit\n3.Search Employee\n4.Update Rating\n5.Display Employees\n6.Exit\nEnter your choice: ";
     cin>>input;
-    int x,y,z;
+    int x,y,z,rating;
+    string name;
+
 while(input!=6){
     switch(input){
         case 1:
-        cout<<"enter the ID: ";
-        cin>>n;
-        insert_node(&root,n);
-        AVL_tree(&root,n);
+        cout<<"enter the ID Name Rating(0-10): ";
+        cin>>n>>name>>rating;
+        insert_node(&root,n,name,rating);
+        AVL_tree_insertion(&root,n);
+        cout<<"Employee added successfully";
             break;
         case 2:
         
-        cout<<"Enter the value: ";
+        cout<<"Enter employee leaving company: ";
         cin>>x;
-       // delete_node2(&root,x);
+       delete_node2(&root,x);
+       AVL_tree_deletion(&root);
+       cout<<"Employee record removed";
             break;
         case 3:
         cout<<"Employee ID to search: ";
@@ -181,14 +268,15 @@ while(input!=6){
         search_ID(root,y);
         break;
         case 4:
-        cout<<"Employee ID and rating: ";
+        cout<<"Employee ID and new rating: ";
         cin>>y>>z;
+        update_rating(&root,y,z);
         break;
         case 5:
         display_tree_inorder(root);
         break;
 }
-cout<<"\n\n\n1.Employee Join\n2.Employee Exit\n3.Search Employee\n4.Update Rating\n5.Display Employees\n6.Exit\nEnter your choice: ";
+cout<<"\nAVL Tree Based Employee Perfomane Management System Menu\n1.Employee Join\n2.Employee Exit\n3.Search Employee\n4.Update Rating\n5.Display Employees\n6.Exit\nEnter your choice: ";
                cin>>input;
 }
 delete_tree(&root);
