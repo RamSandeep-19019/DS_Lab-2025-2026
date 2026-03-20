d23bfc2f2739ef4f147b5016fdea323e74366940
Gumma Ram Sandeep <ramsandeepgumma@gmail.com>
2026-03-20 20:14:15 +0530

    Update quick_sort.cpp
    mostly completed some errors are there

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -18,43 +18,62 @@ struct node* createNode(int id,string name,float marks){
     new_node->next=NULL;
     return new_node;
 }
-void create_lnk_list(node*head,int n){
+void create_lnk_list(node**head,int n){
  string name;
  int id;
  float marks;
- node*temp;
+ node**temp;
+ temp=head;
     for(int i=0;i<n;i++){
         cout<<"Roll_No,name,marks: ";
         cin>>id>>name>>marks;
-        if(head==NULL){
-            head=createNode(id,name,marks);
+        if(*head==NULL){
+            *head=createNode(id,name,marks);
+            temp=head;
         }
         else{
-            temp->next=createNode(id,name,marks);
-            temp=temp->next;
+            (*temp)->next=createNode(id,name,marks);
+            *temp=(*temp)->next;
         }
     }
 }
+void backward_traversal(int pointer,int v[],int arr_size);
 void forward_traversal(int pointer,int v[],int arr_size){
    
     for(int i=0;i<pointer;i++){
         if(v[i]>v[pointer]){
+            cout<<"swapping"<<v[i]<<v[pointer]<<endl;
             swap(v[i],v[pointer]);
             pointer=i;
            break;
         }
-    } backward_traversal(pointer,v,arr_size);
+    }cout<<"going for backward traversal"<<endl;
+     backward_traversal(pointer,v,arr_size);
 }
 void backward_traversal(int pointer,int v[],int arr_size){
     for(int i=arr_size-1;i>pointer;i--){
         if(v[i]>v[pointer]){
+            cout<<"swapping"<<v[i]<<v[pointer]<<endl;
             swap(v[i],v[pointer]);
             pointer=i;
             break;
         }
-    }forward_traversal(pointer,v,arr_size);
+    }cout<<"going for forward traversal"<<endl;
+    forward_traversal(pointer,v,arr_size);
+}
+int search_value(int arr[],int n,int id){
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]==id){
+            return i;
+        }
+    }
+    
 }
 void quick_sort(node* start,node* last){
+    if(start==last||start==NULL){
+        return;
+    }
     node*temp;
     temp=start;
     vector<int> v;
@@ -70,13 +89,57 @@ void quick_sort(node* start,node* last){
     }
     int pointer=n-1;
     int pivot=arr[n-1];
+    cout<<"going for forward traversal"<<endl;
     forward_traversal(pointer,arr,n);
+    node *next_start,*next_last;
+     x=search_value(arr,n,pointer);
+    temp=start;
+    for(int i=0;i<n;i++){
+        temp->marks=arr[i];
+        temp=temp->next;        
+    }
+    temp=start;
+    for (int i = 0; i < x; i++)
+    {
+        next_last=temp;
+        temp=temp->next;
+    }
+    next_start=temp->next;
+    if(next_last!=NULL)
+    quick_sort(start,next_last);
+    if(next_start!=NULL)
+    quick_sort(next_start,last);
     
     
 }
+void display_lnk_list(node *head){
+   node*temp;
+   temp=head;
+    while(temp!=NULL){
+        cout<<temp->roll_No<<" "<<temp->name<<" "<<temp->marks<<endl;
+    }
+}
+void delete_lnk_list(node *head){
+    node *temp;
+    
+    while(head!=NULL){
+        temp=head;
+        head=head->next;
+        delete(temp);
+    }
+}
 int main(){
-    node* head;
+    node *head,*temp;
     head=NULL;
     int n;
-
+    cin>>n;
+    temp=head;
+    create_lnk_list(&head,n);
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    display_lnk_list(head);
+    quick_sort(head,temp);
+    display_lnk_list(head);
+    delete_lnk_list(head);
 }
